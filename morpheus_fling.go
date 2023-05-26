d7f7091574557e7d71ab33e9b77ca6c4357eb5be
Waqas <wabbas@morpheusdata.com>
2023-05-26 14:54:37 +0100

    minor change

--- morpheus_fling.go
+++ morpheus_fling.go
@@ -225,7 +225,7 @@ func main() {
 	esHealth := elasticing.ElasticHealth()
 	esIndices := elasticing.ElasticIndices()
 	esWaterMarkSettings := elasticing.ElasticWatermarkSettings()
-	fmt.Println(esWaterMarkSettings)
+	fmt.Println("Watermark = "+esWaterMarkSettings)
 	rabbitStuff := rabbiting.RabbitStats("morpheus", rmqpassword)
 
 	morpheus, err := ioutil.ReadFile(*logfilePtr)

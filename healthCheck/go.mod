c42e7f2c625d6bf497bf77e0bb13e5479dc70ac8
Waqas <wabbas@morpheusdata.com>
2023-06-23 16:30:37 +0100

    push changes

--- healthCheck/go.mod
+++ healthCheck/go.mod
@@ -1,10 +1,3 @@
-module healthCheck.go
+module healthCheck
 
 go 1.20
-
-require github.com/wabbas-morpheus/morpheus-fling/elasticIng v0.0.0-20230623105021-a7a7b9cbf712
-
-require (
-	github.com/elastic/go-elasticsearch/v7 v7.17.7 // indirect
-	github.com/mitchellh/mapstructure v1.5.0 // indirect
-)
--- morpheus_fling.go
+++ morpheus_fling.go
@@ -18,7 +18,7 @@ import (
 	filereader "github.com/wabbas-morpheus/morpheus-fling/fileReader"
 	portscanner "github.com/wabbas-morpheus/morpheus-fling/portScanner"
 	sysgatherer "github.com/wabbas-morpheus/morpheus-fling/sysGatherer"
-	healthcheck "healthcheck"
+	healthcheck "morpheus-fling/healthcheck"
 	"github.com/mholt/archiver"
 	"github.com/zcalusic/sysinfo"
 	"golang.org/x/text/cases"

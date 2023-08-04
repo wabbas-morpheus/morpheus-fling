214cf7dc1121b3b01b3ef4323a5ae601fbc938aa
Waqas <wabbas@morpheusdata.com>
2023-08-04 16:50:08 +0100

    push changes

--- rbParse/rbParse.go
+++ rbParse/rbParse.go
@@ -1,34 +1,37 @@
 package rbParse
 
-import(
-"fmt"
-"os"
-"bufio"
-"log"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
 )
 
-
-func ParseRb(rbfilePtr string){
+func ParseRb(rbfilePtr string) {
 
 	// Open our rbfile
 	morpheusRBFile, err := os.Open(rbfilePtr)
 	if err != nil {
-        fmt.Println(err)
-    }
-    fmt.Printf("Successfully Opened Morpheus rb file @ %s\n",rbfilePtr)
-    // defer the closing of our jsonFile so that we can parse it later on
-    defer morpheusRBFile.Close()
+		fmt.Println(err)
+	}
+	fmt.Printf("Successfully Opened Morpheus rb file @ %s\n", rbfilePtr)
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer func(morpheusRBFile *os.File) {
+		err := morpheusRBFile.Close()
+		if err != nil {
+			log.Fatalf("defer file error: %v", err)
+		}
+	}(morpheusRBFile)
 
-    // byteValue, _ := ioutil.ReadAll(morpheusRBFile)
+	// byteValue, _ := ioutil.ReadAll(morpheusRBFile)
 
-    sc := bufio.NewScanner(morpheusRBFile)
-    for sc.Scan() {
-        fmt.Println(sc.Text())  // GET the line string
-    }
-    if err := sc.Err(); err != nil {
-        log.Fatalf("scan file error: %v", err)
-        return
-    }
+	sc := bufio.NewScanner(morpheusRBFile)
+	for sc.Scan() {
+		fmt.Println(sc.Text()) // GET the line string
+	}
+	if err := sc.Err(); err != nil {
+		log.Fatalf("scan file error: %v", err)
+		return
+	}
 
-	 
 }

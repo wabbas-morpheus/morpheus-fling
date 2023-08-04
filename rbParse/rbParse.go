c1aaeb77a95f1300c22825f9e993e4b20a2912df
Waqas <wabbas@morpheusdata.com>
2023-08-04 15:36:51 +0100

    push changes

--- rbParse/rbParse.go
+++ rbParse/rbParse.go
@@ -7,5 +7,25 @@ import(
 
 func ParseRb(rbfilePtr string){
 
-	 fmt.Printf("Reading Morpheus rb file @ %s\n",rbfilePtr)
+	// Open our rbfile
+	morpheusRBFile, err := os.Open(rbfilePtr)
+	if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Printf("Successfully Opened Morpheus rb file @ %s\n",rbfilePtr)
+    // defer the closing of our jsonFile so that we can parse it later on
+    defer morpheusRBFile.Close()
+
+    // byteValue, _ := ioutil.ReadAll(morpheusRBFile)
+
+    sc := bufio.NewScanner(morpheusRBFile)
+    for sc.Scan() {
+        _ = sc.Text()  // GET the line string
+    }
+    if err := sc.Err(); err != nil {
+        log.Fatalf("scan file error: %v", err)
+        return
+    }
+
+	 
 }

8b1e3f8368a334c7dc452375c53f85bab1c17ba2
Waqas <wabbas@morpheusdata.com>
2023-08-07 18:25:58 +0100

    push changes

--- rbParse/analyzeRB.go
+++ rbParse/analyzeRB.go
@@ -1,14 +1,10 @@
 package rbParse
 
-import (
-	"fmt"
-)
-
 func GetApplianceInstallType(rbPtr string) string {
 	installType := "AIO"
 	rb := ParseRb(rbPtr)
 	for k, v := range rb {
-		fmt.Printf("setting = %s value = %s\n", k, v)
+		//fmt.Printf("setting = %s value = %s\n", k, v)
 		if k == "mysql['enable']" && v == "true" {
 			installType = "HA"
 		}

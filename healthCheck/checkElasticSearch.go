5a26b909e1b13f97e44a64869a2c9d1ab6fdb3e2
Waqas <wabbas@morpheusdata.com>
2024-01-26 21:13:40 +0000

    moved func GetStorage from sysGathere to ElasticHealthCheck

--- healthCheck/checkElasticSearch.go
+++ healthCheck/checkElasticSearch.go
@@ -1,9 +1,14 @@
 package healthCheck
 
 import (
+	"bufio"
 	"fmt"
 	elasticing "github.com/wabbas-morpheus/morpheus-fling/elasticIng"
-	sysgatherer "github.com/wabbas-morpheus/morpheus-fling/sysGatherer"
+	"log"
+	"os/exec"
+	"strings"
+
+	//sysgatherer "github.com/wabbas-morpheus/morpheus-fling/sysGatherer"
 	"strconv"
 )
 
@@ -81,7 +86,7 @@ func checkESWatermarkThreshold() Check {
 
 	//get total used storage from the app node. Need to find another way to get current storage as the sysinfo not
 	//compatible with other OS
-	currentStorage := sysgatherer.GetStorageUsed()
+	currentStorage := GetStorageUsed()
 
 	// fmt.Println("Low = " + strconv.Itoa(lowNumberOnly))
 	// fmt.Println("High = " + strconv.Itoa(highNumberOnly))
@@ -113,4 +118,54 @@ func checkESWatermarkThreshold() Check {
 
 	return c
 
+}
+
+func GetStorageUsed() int {
+
+	//get path of the elasticsearch data
+	outESFile, err := exec.Command("grep", "data", "/opt/morpheus/embedded/elasticsearch/config/elasticsearch.yml").Output()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	outputES := string(outESFile[:])
+	fPath := ""
+	scannerES := bufio.NewScanner(strings.NewReader(outputES))
+	for scannerES.Scan() { //iterate of each line
+		line := strings.Fields(scannerES.Text()) //convert line text in a list
+		fPath = line[1]                          //get storage used info
+		//fmt.Printf("fpath = %s\n",fPath)
+
+	}
+	if err := scannerES.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	//Get available storage info on elasticsearch mount point
+	out, err := exec.Command("df", "-h", fPath).Output()
+
+	// if there is an error with our execution
+	// handle it here
+	if err != nil {
+		log.Fatal(err)
+	}
+	// as the out variable defined above is of type []byte we need to convert
+	// this to a string or else we will see garbage printed out in our console
+	// this is how we convert it to a string
+	output := string(out[:])
+	used := ""
+	scanner := bufio.NewScanner(strings.NewReader(output))
+	for scanner.Scan() { //iterate of each line
+		line := strings.Fields(scanner.Text()) //convert line text in a list
+		storageUsedPercent := line[4]          //get storage used info
+		used = storageUsedPercent[0 : len(storageUsedPercent)-1]
+
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	rtn, err := strconv.Atoi(used) //Convert to integer
+	fmt.Println("Storage used= " + used)
+	return rtn
 }
--- sysGatherer/sysGatherer.go
+++ sysGatherer/sysGatherer.go
@@ -1,16 +1,10 @@
 package sysgatherer
 
 import (
+	"github.com/zcalusic/sysinfo"
 	"log"
 	"os/user"
-	"os/exec"
-    "bufio"
-    "strings"
-    "strconv"
-	"github.com/zcalusic/sysinfo"
-    
 )
-
 
 // SysGather gathers system statistics and returns them as a string
 func SysGather() *sysinfo.SysInfo {
@@ -29,58 +23,4 @@ func SysGather() *sysinfo.SysInfo {
 
 	return &si
 
-	
-}
-
-func GetStorageUsed() int{
-
-    //get path of the elasticsearch data  
-    outESFile, err := exec.Command("grep","data","/opt/morpheus/embedded/elasticsearch/config/elasticsearch.yml").Output()
-
-    if err != nil {
-        log.Fatal(err)
-    }
-
-
-    outputES := string(outESFile[:])
-    fPath := ""
-    scannerES := bufio.NewScanner(strings.NewReader(outputES))
-        for scannerES.Scan() { //iterate of each line
-                line := strings.Fields(scannerES.Text())//convert line text in a list
-                fPath = line[1] //get storage used info
-                //fmt.Printf("fpath = %s\n",fPath)
-                
-                        
-                
-        }
-        if err := scannerES.Err(); err != nil {
-                log.Fatal(err)
-        }
-
-    //Get available storage info on elasticsearch mount point
-    out, err := exec.Command("df","-h",fPath).Output()
-
-    // if there is an error with our execution
-    // handle it here
-    if err != nil {
-        log.Fatal(err)
-    }
-    // as the out variable defined above is of type []byte we need to convert
-    // this to a string or else we will see garbage printed out in our console
-    // this is how we convert it to a string
-    output := string(out[:])
-    used := ""
-    scanner := bufio.NewScanner(strings.NewReader(output))
-        for scanner.Scan() { //iterate of each line
-                line := strings.Fields(scanner.Text())//convert line text in a list
-                storageUsedPercent := line[4] //get storage used info
-                used = storageUsedPercent[0:len(storageUsedPercent)-1]
-                        
-                
-        }
-        if err := scanner.Err(); err != nil {
-                log.Fatal(err)
-        }
-        rtn, err :=  strconv.Atoi(used)//Convert to integer
-return rtn
 }

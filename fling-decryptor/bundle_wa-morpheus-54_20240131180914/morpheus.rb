7ea0bf2302362dc5e84dd1cfe0fe3207acd6a4ee
Waqas <wabbas@morpheusdata.com>
2024-01-31 18:22:43 +0000

    Minor changes

--- fling-decryptor/bundle_wa-morpheus-54_20240131180914/morpheus.rb
+++
@@ -1,5 +0,0 @@
-## Url on which Morpheus will be reachable.
-## For more details on configuring appliance_url see:
-## https://docs.morpheusdata.com
-appliance_url 'https://wa-morpheus-54.test.morpheusdata.com'
-
